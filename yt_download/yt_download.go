850c0c3eb1d330e26ebd4e5020c07da258507b78
kendall <kendall@kendalls-Mini.hsd1.ca.comcast.net>
2023-10-10 01:19:05 -0700

    added if then statement to yt_download.go

--- yt_download/yt_download.go
+++ yt_download/yt_download.go
@@ -3,11 +3,25 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 	"os/exec"
 	"path/filepath"
 )
 
 func main() {
+
+	if _,err := os.Stat("Jungkook (정국) '3D (Feat. Jack Harlow)' Lyrics [C8vcCplJb1c].mp3");
+		err == nil {
+			fmt.Println("File exist, download will not run")
+			
+		} else {
+		fmt.Println("File does not exist. File will now be downloaded.")
+		dl()
+	}
+
+}
+
+func dl(){
 	path := "/Users/kendall/repo/utils_personal"
 	ytScript := "yt"
 	ytPath := filepath.Join(path, ytScript)
@@ -17,11 +31,9 @@ func main() {
 	ytOpt2 := "-x"
 	url := "https://www.youtube.com/watch?v=C8vcCplJb1c&pp=ygURanVuZ2tvb2sgM2QgYXVkaW8%3D"
 
-	out, err := exec.Command(ytPath, ytOpt1, ytOpt2, url).Output()
+	_, err := exec.Command(ytPath, ytOpt1, ytOpt2, url).Output()
 
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	fmt.Println("Out: %s", out)
 }
