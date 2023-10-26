dcb8febfef16c64e04d44398d902786bdba29bdf
Danny Hur <dannyhur@MBP14.local>
2023-10-26 07:43:36 -0700

    addition

---
+++ tutorial/pointers_return_pointer.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+type person struct {
+	name string
+	age  int
+}
+
+func initPerson() *person {
+	m := person{name: "noname", age: 50}
+	return &m
+}
+
+func changeName(name *string) {
+	*name = "March"
+}
+
+func main() {
+	x := "Danny"
+	fmt.Println(x)
+	changeName(&x)
+	fmt.Println(x)
+
+	// fmt.Printf("%p\n", initPerson())
+	// fmt.Printf("%T\n", initPerson())
+}
