973df415159bd9229c79d22ad2cdee4e6f62736f
Danny Hur <dannyhur@MBP14.local>
2023-10-28 10:45:01 -0700

    updated golang

--- tutorial/mult_val_return.go
+++ tutorial/mult_val_return.go
@@ -1,15 +1,18 @@
 package main
 
-import "fmt"
+import (
+	f "fmt"
+)
 
-func ret(a,b int) (int,int) {
-	aa := a*2
-	bb := b*2
-	return aa,bb
+func ret(a, b int) (int, int) {
+	aa := a * 2
+	bb := b * 2
+	return aa, bb
 }
 
 func main() {
-	sum1,sum2 := ret(1,3)
-	//fmt.Println("sum1 ",sum1)
-	fmt.Println("sum1 of 1,3 is ",sum1,sum2)
+	val1 := 1
+	val2 := 3
+	result1, result2 := ret(val1, val2)
+	f.Printf("ret(%d, %d): %d, %d\n", val1, val2, result1, result2)
 }
--- tutorial/range.go
+++ tutorial/range.go
@@ -3,23 +3,34 @@ package main
 import "fmt"
 
 func main() {
-	nums:=[]int{1,2,3,4}
-	kvs:=map[string]string{"name":"Danny","age":"47"}
-	
-	for i := range nums{
-		fmt.Println("Index:",i)
+	nums := []int{1, 2, 3, 4}
+	kvs := map[string]string{"name": "Danny", "age": "47", "hobby": "guitar"}
+
+	for i := range nums {
+		fmt.Println("Index:", i)
 	}
 
 	fmt.Println()
 
-	for k,v := range kvs{
-		fmt.Printf("%s -> %s\n",k,v)
+	for k, v := range kvs {
+		if k == "hobby" {
+			if v == "motorcycling" {
+				fmt.Println("hobby is ", v)
+			} else {
+				fmt.Println("could not establish a hobby")
+			}
 		}
 
-	fmt.Println("kvs:",kvs)
-
-	for k := range kvs{
-		fmt.Printf("key: %s\n",k)
+		for k, v := range kvs {
+			fmt.Printf("%s -> %s\n", k, v)
 		}
 
+		// fmt.Println("kvs:", kvs)
+
+		fmt.Println()
+		for k := range kvs {
+			fmt.Printf("key: %s\n", k)
+		}
+
+	}
 }
