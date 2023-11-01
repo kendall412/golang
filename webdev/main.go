35e28eb56006b86d3a163ed8f7761800b7e9fd6a
Danny Hur <dannyhur@MBP14.local>
2023-11-01 08:23:25 -0700

    changed playlistDL.go to main.go

---
+++ webdev/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	f "fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/hello", helloHandleFunc)
+	http.ListenAndServe(":8080", nil)
+}
+
+func helloHandleFunc(w http.ResponseWriter, r *http.Request) {
+	f.Fprint(w, "Hello, Danny!")
+}
