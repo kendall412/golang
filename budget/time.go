ab3953d9c8c0cde36ab40b135789b1bc620ab703
Danny Hur <fourier412@gmail.com>
2024-02-08 11:53:33 -0800

    Create time.go

---
+++ budget/time.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"time"
+)
+
+func convert_string_date(date_str []string) []time.Time {
+	time_date := []time.Time{}
+	for _, date_el := range date_str {
+		date, _ := time.Parse("1/2/2006", date_el)
+
+		// fmt.Println(date)
+		// fmt.Printf("%T\n", date)
+
+		// fmt.Printf("Year: %d\n", date.Year())
+		// fmt.Printf("Month: %s\n", date.Month().String())
+		// fmt.Printf("Day: %d\n\n", date.Day())
+		time_date = append(time_date, date)
+	}
+	return time_date
+}
