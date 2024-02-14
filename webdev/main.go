ad7c7d70501da56c26eca183606cd39e5cdf86e9
Danny Hur <kendall@Dannys-MacBook-Pro-2.local>
2024-02-14 09:28:11 -0800

    added to growth_adept_web_dev

--- webdev/main.go
+++ webdev/main.go
@@ -1,21 +1,30 @@
 package main
 
 import (
-	f "fmt"
+	"html/template"
 	"net/http"
 )
 
+var tpl *template.Template
+
 func main() {
-	http.HandleFunc("/hello", helloHandleFunc)
+
+	// tpl, _ = template.ParseGlob("templates/*.html")
+	tpl, _ = template.ParseFiles("templates/index")
+
+	http.HandleFunc("/", indexHandler)
+
 	/* http.ListenAndServe:
 
 	implicitly knows :8080 really means localhost:8080
 
 	entering "nil" implicitly uses DefaultServeMux
 	ServeMux is an HTTP request multiplexer. It matches the URL of each incoming request against a list of registered patterns and calls teh handler for the pattern that most closely matches the URL.*/
+
 	http.ListenAndServe(":8080", nil)
 }
 
-func helloHandleFunc(w http.ResponseWriter, r *http.Request) {
-	f.Fprint(w, "Hello, Danny!")
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	// f.Fprint(w, "Hello, Danny!")
+	tpl.Execute(w, nil)
 }
